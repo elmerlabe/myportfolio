19e4551e46859083c4bf2bd218c9631b0c4fbf24
elmer <elmerlabe@gmail.com>
2022-05-23 23:03:08 +0800

    added some description

---
+++ assets/js/myjs.js
@@ -0,0 +1,16 @@
+var project = ["SCATHZ", "Creativchimp", "AI Clock", "Kiss Puppy", "Equipment Inventory", "Whitehat Wifi", "Comfortroom Monitoring", "Temperature Logger", "People Counter & Limter" ];
+var projectLink = ["https://dcc-aguila.appspot.com/", "https://creativchimp.netlify.app/", "", "https://kisspuppy.netlify.app/", "", "", "", ""];
+
+
+$(document).ready(function(){
+
+
+});
+
+const showModal = (title) => {
+    $("#myModal").show()
+}
+
+$(".close").click(() =>{
+    $("#myModal").hide()
+});
